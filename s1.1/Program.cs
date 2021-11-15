e13e14f057e46cf08cd9b1dadbea3e652f7cae79
kris.o <yajkakusta@gmail.com>
2021-11-15 17:46:52 +0500

    удалила прошлые файлы(не работал console./name/), создала в общем файле

--- s1.1/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.Clear();
-double a = 2.23;
-System.Console.WriteLine("Вывод числа с учетом сотых");
-System.Console.WriteLine(a);
--- s1.2/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.Clear();
-double e = 2.7;
-System.Console.WriteLine("Вывести на экран число 'e' (основание натурального логарифма) с точностью до 10-ых");
-System.Console.WriteLine(e);
