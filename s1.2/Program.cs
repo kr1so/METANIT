cc2deb988abedff59d3653c55c1772bc20441e91
kris.o <yajkakusta@gmail.com>
2021-11-15 15:39:37 +0500

    work with deBug

--- s1.2/Program.cs
+++ s1.2/Program.cs
@@ -1,3 +1,4 @@
-﻿double e = 2.7;
+﻿Console.Clear();
+double e = 2.7;
 System.Console.WriteLine("Вывести на экран число 'e' (основание натурального логарифма) с точностью до 10-ых");
 System.Console.WriteLine(e);
