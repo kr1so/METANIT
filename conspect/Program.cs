d951eabcd2e2e3669670ca8aee4ab7b80cbbc876
kris.o <yajkakusta@gmail.com>
2021-11-17 19:45:15 +0500

    создала файл с конспектами

---
+++ conspect/Program.cs
@@ -0,0 +1,23 @@
+﻿//2 вида разных переменных типа string
+/* string name;
+string Name; */
+/* string name;
+name = "Tom";
+System.Console.WriteLine(name); */
+//пример инициализации(когда сразу при определении испольузется присваивание значения переменной)
+/* string Name = "Tedd";
+System.Console.WriteLine(Name); */
+//пример того, как значение переменной можно менять многократно
+/* System.Console.WriteLine("Введите имя:");
+string name = "Monica";
+System.Console.WriteLine(name);
+name = "Edvard";
+System.Console.WriteLine(name);
+name = "Tom";
+System.Console.WriteLine(name);
+name = "Anastasiya";
+System.Console.WriteLine(name); */
+//КОНСТАНТЫ
+//обозначаются словом 'const' перед типом данных. всегда неизменны
+/* const string name = "Tom";
+System.Console.WriteLine(name); */
