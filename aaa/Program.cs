aa749f03e41ba389df45961195ad26fc2a6f4bbe
kris.o <yajkakusta@gmail.com>
2021-11-19 15:41:31 +0500

    пофиксила индексацию массива

--- aaa/Program.cs
+++ aaa/Program.cs
@@ -23,16 +23,22 @@ Print_Array(arrayA);
 System.Console.WriteLine();
  */
 
- int Fill_Array (int [] array_fill, int min) 
+ int Fill_Array (int[] array_fill, int min) 
 {
     int length = array_fill.Length;
-    for(int i=0; i < length; i++)
+    int i = 0;
+    if (i<length)
     {
         array_fill[i] = min + 1;
+        i++;
     }
-    return array_fill;
+    /* for(int i=0; i < length; i++)
+    {
+        array_fill[i] = min + 1;
+    } */
+    return array_fill[i];
 }
-void Print_Array(int [] array_print)
+void Print_Array(int[] array_print)
 {
     int length_print = array_print.Length;
     for(int j = 0; j < length_print; j++)
@@ -41,8 +47,9 @@ void Print_Array(int [] array_print)
     }
 }
 Console.Clear();
-int[] number = new int [90];
-Fill_Array(number, 2);
+int[] number = new int [10];
+int min_Value = 2;
+Fill_Array(number, min_Value);
 int b = 0;
 while ( (number[b] * number[b]) < number.Length)
 {
@@ -54,6 +61,5 @@ while ( (number[b] * number[b]) < number.Length)
         }
     }
     b++;
-    
 }
 Print_Array(number);
